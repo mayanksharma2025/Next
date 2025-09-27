d0bf67c0789f4b4183aed00bd894059debdf45f4
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-09-27 13:21:08 +0530

    basics concept

---
+++ pokemon-nextjs/pages/index.js
@@ -0,0 +1,61 @@
+// import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+// export async function getServerSideProps(context) {
+//   const pokemonResp = await fetch(
+//     "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
+//   );
+//   const pokemon = await pokemonResp.json();
+//   return {
+//     props: {
+//       pokemon,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   return { paths: [], fallback: true };
+// }
+
+export async function getStaticProps(context) {
+  const pokemonResp = await fetch(
+    "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
+  );
+  const pokemon = await pokemonResp.json();
+  return {
+    props: {
+      pokemon,
+    },
+  };
+}
+
+export default function Home({ pokemon }) {
+  // const [pokemon, setPokemon] = useState([]);
+
+  // useEffect(() => {
+  //   async function getPokemon() {
+  //     const pokemonResp = await fetch(
+  //       "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
+  //     );
+  //     setPokemon(await pokemonResp.json());
+  //   }
+  //   getPokemon();
+  // }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>Pokemon Home Page</title>
+      </Head>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon, index) => (
+          <Link href={`/pokemon/${pokemon.id}`} key={pokemon.id}>
+            {pokemon.name.english}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
