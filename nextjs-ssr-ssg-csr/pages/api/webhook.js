926d9876726a69153b3baa32bc7bfe3b529d4830
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-09-29 14:26:57 +0530

    PIPELINE

--- nextjs-ssr-ssg-csr/pages/api/webhook.js
+++ nextjs-ssr-ssg-csr/pages/api/webhook.js
@@ -5,7 +5,7 @@
 export default async function handler(req, res) {
   const SECRET_TOKEN = process.env.REVALIDATE_SECRET
 
-  if (req.query.secret !== SECRET_TOKEN) {
+  if (req.query.secret !== 'PIPELINE') {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
