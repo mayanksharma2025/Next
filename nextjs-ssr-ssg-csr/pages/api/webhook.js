4b116df8242fb66caa6f675a974a50f24cd9b370
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-09-29 12:47:07 +0530

    webhook with secret key

--- nextjs-ssr-ssg-csr/pages/api/webhook.js
+++ nextjs-ssr-ssg-csr/pages/api/webhook.js
@@ -3,8 +3,18 @@
 // https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
 
 export default async function handler(req, res) {
-  for (const url of req.body) {
-    await res.unstable_revalidate(url)
+  const SECRET_TOKEN = process.env.REVALIDATE_SECRET
+
+  if (req.query.secret !== SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
   }
-  res.status(200).json({ revalidate: true })
+
+  try {
+    for (const url of req.body) {
+      await res.revalidate(url)
+    }
+    return res.status(200).json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).json({ message: 'Error revalidating', error: err })
+  }
 }
