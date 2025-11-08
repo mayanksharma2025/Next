5427080cc6e50bb2f73436afa63eda8f3288eeaa
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-11-08 21:25:40 +0530

    route-handler changes

--- Next.js-14-Tutorials/route-handlers-demo/src/app/profile/page.tsx
+++ Next.js-14-Tutorials/route-handlers-demo/src/app/profile/page.tsx
@@ -1,11 +1,15 @@
 import { cookies } from 'next/headers'
 
 export default function ProfilePage() {
-  const data = cookies().get('30041995') as string | undefined
+  const data = cookies().get('30041995') as {
+    name: string
+    value: string
+  } | null
   return (
-    <div>
+    <div className="relative min-h-[50rem]">
       Profile details
-      <h1>{data as string}</h1>
+      <h1>{data?.name}</h1>
+      <h1 className="text-3xl text-slate-400 text-uppercase">{data?.value}</h1>
     </div>
   )
 }
