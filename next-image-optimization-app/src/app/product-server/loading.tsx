3bd76efcbf519c119bcb1efb1bf4f6829e0d89f4
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-11-29 19:58:35 +0530

    Loading fix Cls

--- next-image-optimization-app/src/app/product-server/loading.tsx
+++ next-image-optimization-app/src/app/product-server/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+    <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 m-24">
       {Array.from({ length: 6 }).map((_, i) => (
-        <div key={i} className="h-64 bg-gray-200 rounded-xl animate-pulse" />
+        <div key={i} className="h-96 bg-gray-200 rounded-xl animate-pulse" />
       ))}
     </div>
   )
