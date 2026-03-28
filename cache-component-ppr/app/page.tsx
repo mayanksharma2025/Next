32c421afd3cbea494e7cd63fb59ddc4eb72cdc5d
Mayank Sharma <mayankece155@gmail.com>
2026-03-28 14:07:40 +0530

    UI-Level Caching

--- cache-component-ppr/app/page.tsx
+++ cache-component-ppr/app/page.tsx
@@ -1,14 +1,30 @@
-// app/page.tsx
-import { getUsers } from "./lib/api";
+// app/posts/page.tsx
+import { cacheLife } from "next/cache";
 
-export default async function Page() {
-  const users = await getUsers();
+export default async function PostsPage() {
+  "use cache";
+  cacheLife("hours");
+
+  const res = await fetch("http://localhost:4000/posts");
+  const posts = await res.json();
 
   return (
-    <ul>
-      {users.map((u: any) => (
-        <li key={u.id}>{u.name}</li>
+    <div>
+      <h1>Posts</h1>
+      {posts.map((p: any) => (
+        <div key={p.id}>{p.title}</div>
       ))}
-    </ul>
+    </div>
   );
+}
+
+{
+  /*
+      🧠 Difference        vs       Data-level
+      
+      Data-level	                   UI-level
+      reusable	                     tied to component
+      flexible	                     simple
+      best for shared logic	         best for page-level caching
+  */
 }
